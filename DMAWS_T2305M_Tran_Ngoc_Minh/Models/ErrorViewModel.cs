c62e5a396d9fc47d53c58cc08ddc039ff6c10198
otoshi kami <kirito2932000@gmail.com>
2024-10-08 08:55:54 +0700

    Add project files.

---
+++ DMAWS_T2305M_Tran_Ngoc_Minh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DMAWS_T2305M_Tran_Ngoc_Minh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
